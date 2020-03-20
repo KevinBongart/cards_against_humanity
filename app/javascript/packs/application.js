fd11de4698ebee957c1998772951c39d6f58eb8c
Alexandre Testu <alexandre@testu.fr>
2020-03-20 13:16:34 -0400

    swipin' cards

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,3 +17,36 @@ require("channels")
 // const imagePath = (name) => images(name, true)
 
 import "controllers"
+import * as Swing from "swing"
+
+document.addEventListener('turbolinks:load', function() {
+  // Prepare the cards in the stack for iteration.
+  const cards = [].slice.call(document.querySelectorAll('#hand .card'));
+
+  // An instance of the Stack is used to attach event listeners.
+  const stack = Swing.Stack();
+
+  cards.forEach((targetElement) => {
+    // Add card element to the Stack.
+    stack.createCard(targetElement);
+    targetElement.classList.add('in-deck');
+  });
+
+  // Add event listener for when a card is thrown out of the stack.
+  stack.on('throwout', (event) => {
+    // e.target Reference to the element that has been thrown out of the stack.
+    // e.throwDirection Direction in which the element has been thrown (Direction.LEFT, Direction.RIGHT).
+
+    var xhr = new XMLHttpRequest();
+    // /games/EKFQS/round/next_card_in_hand
+    xhr.open("POST", location.pathname+'/next_card_in_hand');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('X-CSRF-Token', document.getElementsByName('csrf-token')[0].content);
+    xhr.send();
+  });
+
+  stack.on('throwoutend', (event) => {
+    stack.getCard(event.target).throwIn()
+    event.target.parentNode.appendChild(event.target)
+  })
+});
