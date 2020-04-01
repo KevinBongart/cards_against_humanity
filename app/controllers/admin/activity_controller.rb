7c18f2cbe40ba402a93741e99081ec5b62bf1fed
Kevin Bongart <contact@kevinbongart.net>
2020-04-01 14:42:39 +0200

    Better query

--- app/controllers/admin/activity_controller.rb
+++ app/controllers/admin/activity_controller.rb
@@ -3,9 +3,12 @@ module Admin
     def show
       game_ids = Round.where('updated_at > ?', 1.day.ago).distinct.pluck(:game_id)
 
-      @games = Game.where(id: game_ids).order(created_at: :desc).includes(:rounds, :options).select do |game|
-        game.max_players > 1 && game.rounds.many?
-      end
+      @games = Game
+        .where(id: game_ids)
+        .where('max_players > 1')
+        .order(created_at: :desc)
+        .includes(:rounds, :options)
+        .select { |game| game.rounds.many? }
     end
   end
 end
